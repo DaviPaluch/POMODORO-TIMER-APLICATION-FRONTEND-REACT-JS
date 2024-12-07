b45040467b918a91140e4eda0f3b294c3dd1f91a
davi paluch <davipaluch@icloud.com>
2024-12-07 16:49:54 -0300

    feat: working with themes

--- pomodoro-timer/src/App.tsx
+++ pomodoro-timer/src/App.tsx
@@ -1,16 +1,18 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
 
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary">Enviar</Button>
       <Button variant="secondary">Enviar</Button>
       <Button variant="danger">Enviar</Button>
       <Button variant="success">Enviar</Button>
       <Button >Enviar</Button>
-    </>
+    </ThemeProvider>
   )
 }
 
